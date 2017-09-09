deac066cf9c406736b99ccaea5a31dc431b42250
Atul Kumar <atulsoft@gmail.com>
2017-09-10 00:02:26 +0530

    cleanup :)

---
+++ src/main/java/com/fp/config/FifthPillarMvcConfig.java
@@ -0,0 +1,44 @@
+package com.fp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class FifthPillarMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("fifth_pillar_home");
+        registry.addViewController("/home").setViewName("fifth_pillar_home");
+        registry.addViewController("/mentor").setViewName("pages/mentor");
+        registry.addViewController("/team").setViewName("pages/team");
+        registry.addViewController("/contact").setViewName("Contact");
+        registry.addViewController("/service").setViewName("pages/services");
+        registry.addViewController("/organization-structure").setViewName("pages/organization-structure");
+
+        registry.addViewController("/worldofhonesty/home").setViewName("who/world_of_honesty_home");
+    }
+
+    // @Bean(name = "dataSource")
+    //
+    // public DriverManagerDataSource dataSource() {
+    // DriverManagerDataSource driverManagerDataSource = new
+    // DriverManagerDataSource();
+    // driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+    // driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/userbase");
+    // driverManagerDataSource.setUsername("root");
+    // driverManagerDataSource.setPassword("root");
+    // return driverManagerDataSource;
+    // }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
